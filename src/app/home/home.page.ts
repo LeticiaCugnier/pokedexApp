aa0feb9d92a589438c8db3f858fd4b3ab6a1cf29
Letícia Cúgnier Gonçalves <contato.leticiacugnier@gmail.com>
2020-02-17 22:21:28 -0300

    Adicionado lista dinâmica
    Removido os HTML dos pokemons
    Criado o Array com os pokemons
    Utilizando o for para exibir os dados do Array

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,59 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public listaPokemon = [
+    {
+      numero: 'N°001', nome: 'Bulbasaur', tipos: ['Grass', 'Poison'],
+      img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png' 
+    },
+    {
+      numero: 'N°002', nome: 'Ivysaur', tipos: ['Grass', 'Poison'],
+      img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png' 
+    },
+    {
+      numero: 'N°003', nome: 'Venusaur', tipos: ['Grass', 'Poison'],
+      img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png' 
+    },
+    {
+      numero: 'N°004', nome: 'Charmander', tipos: ['Grass', 'Poison'],
+      img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png' 
+    },
+    {
+      numero: 'N°005', nome: 'Charmeleon', tipos: ['Fire'],
+      img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png' 
+    },
+    {
+      numero: 'N°006', nome: 'Charizard', tipos: ['Fire', 'Flying'],
+      img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png' 
+    },
+    {
+      numero: 'N°007', nome: 'Squirtle', tipos: ['Water'],
+      img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png' 
+    },
+    {
+      numero: 'N°008', nome: 'Wartortle', tipos: ['Water'],
+      img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png' 
+    },
+    {
+      numero: 'N°009', nome: 'Blastoise', tipos: ['Water'],
+      img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png' 
+    },
+    {
+      numero: 'N°025', nome: 'Pikachu', tipos: ['Water'],
+      img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png' 
+    },
+    {
+      numero: 'N°026', nome: 'Raichu', tipos: ['Electric'],
+      img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/026.png' 
+    },
+    {
+      numero: 'Nº172', nome: 'Pichu', tipos: ['Electric'],
+      img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/172.png' 
+    },
+
+  ];
+  public listaFiltrada = [];
+
   constructor() {}
 
 }
